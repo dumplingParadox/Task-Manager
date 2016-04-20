3663b8bdf799db470c0cdd3d54d694ba74395f8f
dumplingParadox <shivam.vijay95@gmail.com>
2016-04-20 07:23:07 +0530

    Update With Sockets and UI enhancements.

---
+++ Task-Manager-Host.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+
+#define MAXPENDING 5    /* Max connection requests */
+          
+void Die(char *mess) { perror(mess); return; }
+
+void HandleClient(int sock) {
+	char buffer[128];
+	int received = -1;
+
+	/* Receive message */
+	if ((received = recv(sock, buffer, 128, 0)) < 0) {
+		Die("Failed to receive initial bytes from client");
+	}
+	//buffer[received]='\0';
+	printf("%s\n",buffer);
+	FILE *fp;	
+	if((fp=popen(buffer,"r"))==NULL){
+					char msg[]="Error opening pipe!\n";
+					send(sock,msg,strlen(msg),0);
+					return;
+				}
+	/* Send bytes and check for more incoming data in loop */
+	/*while (received > 0) {
+		// Send back received data 
+		if (send(sock, buffer, received, 0) != received) {
+			Die("Failed to send bytes to client");
+		}
+
+		// Check for more data 
+		if ((received = recv(sock, buffer, 32, 0)) < 0) {
+			Die("Failed to receive additional bytes from client");
+		}
+	}*/
+
+	while(fgets(buffer,128,fp)!=NULL){
+
+		if (send(sock, buffer, 128, 0) != 128) {
+			Die("Failed to send bytes to client");
+		}
+
+	}
+	strcpy(buffer,"exit");
+	if (send(sock, buffer, 128, 0) != 128) {
+		Die("Failed to send bytes to client");
+	}
+	close(sock);
+}
+
+int main(int argc, char *argv[]) {
+	int serversock, clientsock;
+	struct sockaddr_in echoserver, echoclient;
+	int port=8796;
+	/* Create the TCP socket */
+	if ((serversock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+		Die("Failed to create socket");
+	}
+
+	/* Construct the server sockaddr_in structure */
+	memset(&echoserver, 0, sizeof(echoserver));       /* Clear struct */
+	echoserver.sin_family = AF_INET;                  /* Internet/IP */
+	echoserver.sin_addr.s_addr = htonl(INADDR_ANY);   /* Incoming addr */
+	echoserver.sin_port = htons(port);       /*server port*/
+
+	/* Bind the server socket */
+	if (bind(serversock, (struct sockaddr *) &echoserver,sizeof(echoserver)) < 0) {
+		Die("Failed to bind the server socket");
+	}
+
+	/* Listen on the server socket */
+	if (listen(serversock, MAXPENDING) < 0) {
+		Die("Failed to listen on server socket");
+	}
+
+	/* Run until cancelled */
+	while (1) {
+		unsigned int clientlen = sizeof(echoclient);
+		/* Wait for client connection */
+		if ((clientsock = accept(serversock, (struct sockaddr *) &echoclient,&clientlen)) < 0) {
+			Die("Failed to accept client connection");
+		}
+		fprintf(stdout, "Client connected: %s\n",inet_ntoa(echoclient.sin_addr));
+		HandleClient(clientsock);
+	}
+}
--- Task-Manager.c
+++ Task-Manager.c
@@ -4,7 +4,14 @@ TO COMPILE :
 
 */
 
-#include <gtk/gtk.h>
+#include<gtk/gtk.h>
+#include<stdio.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<netinet/in.h>
 
 //GLOBAL DECLARATIONS.
 
@@ -20,8 +27,8 @@ TO COMPILE :
 
 //Variables for Task-Manager.
 	GtkBuilder *task_manager_ui;
-	GObject *window,*menubar,*menu_file,*menu_view,*menu_options,*menu_help;
-	GObject *file_run,*file_exit;
+	GObject *task_manager_window,*menubar,*menu_file,*menu_view,*menu_options,*menu_help;
+	GObject *file_newcon,*file_run,*file_exit;
 	GObject *options_ontop,*options_minclose,*options_color;
 	GObject *view_refreshnow,*view_refreshspeed;
 	GObject *help_about;
@@ -29,6 +36,72 @@ TO COMPILE :
 
 //GLOBAL DECLARATIONS ENDS.
 
+
+//SOCKETS START
+
+void Die(char *mess) { perror(mess); return; }
+
+void setupSocket(char *ip,int port){
+	printf("Setting up socket.\n");
+	int sock;
+	struct sockaddr_in echoserver;
+	char buffer[128]="0";
+	unsigned int echolen;
+	int received = 0;
+            
+	/* Create the TCP socket */
+	if ((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+		Die("Failed to create socket");
+	}
+
+	/* Construct the server sockaddr_in structure */
+	memset(&echoserver, 0, sizeof(echoserver));       /* Clear struct */
+	echoserver.sin_family = AF_INET;                  /* Internet/IP */
+	echoserver.sin_addr.s_addr = inet_addr(ip);  /* IP address */
+	echoserver.sin_port = htons(port);       /* server port */
+	/* Establish connection */
+	if (connect(sock,(struct sockaddr *) &echoserver,sizeof(echoserver)) < 0) {
+		Die("Failed to connect with server");
+		return;
+	}
+
+	/* Send the word to the server */
+	strcpy(buffer,"whoami | xargs top -b -n 1 -o -COMMAND -u | awk \'{printf \"%-s %6s %-4s %-4s %-4s\\n\",$NF,$1,$9,$10,$2}\'");
+
+	//01 COMMANDS [ Get tasks ] : whoami | xargs top -b -n 1 -o -COMMAND -u | awk '{if(NR>7)printf "%-s %6s %-4s %-4s %-4s\n",$NF,$1,$9,$10,$2}'
+	//02 COMMANDS [ Get CPU% Usage ] : top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print $1"%"}'	
+	//03 COMMANDS [ Get Mem% Usage ] : free -m | grep Mem | awk '{printf("%0.1f%s",$3/$2*100,"%")}'
+	
+	if (send(sock, buffer, 128, 0) != 128) {
+		Die("Mismatch in number of sent bytes");
+		return;
+	}
+
+	/* Receive the word back from the server */
+	while (strcmp(buffer,"exit")!=0) {
+		int bytes = 0;
+		if ((bytes = recv(sock, buffer, 128, 0)) < 1) {
+			Die("Failed to receive bytes from server");
+			return;
+		}
+		received += bytes;
+		buffer[bytes] = '\0';        /* Assure null terminated string */
+		fprintf(stdout, buffer);
+		fprintf(stdout, "\n");
+	}
+
+	fprintf(stdout, "\n");
+	close(sock);
+	return;
+}
+
+//SOCKET ENDS
+
+void closeNewConnectionWindow(GtkWidget *widget,GtkWidget *window){
+	if(window!=NULL)
+		gtk_widget_destroy(window);
+	gtk_widget_set_sensitive(task_manager_window,TRUE);
+}
 
 static void printMenuActivatedData(GtkWidget *widget,gpointer data){
   g_print("\n%s was activated.\n",(gchar *)data);
@@ -50,24 +123,37 @@ int validateIP(char *ip){
 	return 1;
 }
 
-void startConnection(char *ip,char *port){
-	printf("IP : %s\n",ip);
-	loading_connection_window = gtk_builder_get_object(new_connection_ui,"loading-connection-dialogue");
-	g_signal_connect(loading_connection_window,"destroy",G_CALLBACK(gtk_main_quit),NULL);
+void startConnection(char *ip,int port){
+
+	//printf("IP : %s\nPort : %d\n",ip,port);
+	gtk_widget_set_sensitive(task_manager_window,FALSE);
+	loading_connection_window = gtk_dialog_new();
+	GtkWidget *content_area;
+	GtkWidget *box=gtk_builder_get_object(new_connection_ui,"loading-box");
 	
-	loading_connection_cancel=gtk_builder_get_object(new_connection_ui,"cancel-button");
-	g_signal_connect(loading_connection_cancel,"clicked",G_CALLBACK(gtk_main_quit),NULL);
+	loading_connection_cancel=gtk_builder_get_object(new_connection_ui,"loading-cancel-button");
+	g_signal_connect(loading_connection_cancel,"clicked",G_CALLBACK(closeNewConnectionWindow),loading_connection_window);
 
 	loading_connection_spinner=gtk_builder_get_object(new_connection_ui,"loading-spinner");
 	gtk_spinner_start(loading_connection_spinner);
 
-	gtk_widget_show_all(loading_connection_window);
-	//gtk_widget_destroy(new_connection_window);
+	content_area = gtk_dialog_get_content_area(GTK_DIALOG(loading_connection_window));
+	//gtk_widget_reparent(box,GTK_CONTAINER(content_area));
+	gtk_container_add(GTK_CONTAINER(content_area), box);
 
+	g_signal_connect(loading_connection_window, "destroy", G_CALLBACK(closeNewConnectionWindow),loading_connection_window);
+	gtk_window_set_title(loading_connection_window,"Loading Tasks");
+	gtk_window_set_skip_taskbar_hint(loading_connection_window,TRUE);
+	gtk_window_set_resizable(GTK_WINDOW(loading_connection_window), FALSE);
+
+	gtk_widget_show_all(loading_connection_window);
+
+	setupSocket(ip,port);
+	
+	closeNewConnectionWindow(NULL,loading_connection_window);
 }
 
 static void connectToIp(GtkWidget *widget,GtkBuilder *data){
-	GtkBuilder *new_connection_ui = data;
 	GObject *ip_add=gtk_builder_get_object(new_connection_ui,"ip-entry");
 	GObject *port_add=gtk_builder_get_object(new_connection_ui,"port-entry");
 	char *ip=gtk_entry_get_text(GTK_ENTRY(ip_add));
@@ -86,9 +172,9 @@ static void connectToIp(GtkWidget *widget,GtkBuilder *data){
 	
 	if(flagIP&&flagPort){
 		gtk_widget_modify_fg(GTK_WIDGET(ip_add),GTK_STATE_NORMAL,&green);
-		gtk_widget_modify_fg(GTK_WIDGET(port_add),GTK_STATE_NORMAL,&green);
-		gtk_widget_hide(new_connection_window);
-		startConnection(ip,port);		
+		gtk_widget_modify_fg(GTK_WIDGET(port_add),GTK_STATE_NORMAL,&red);
+		startConnection(ip,prt);
+		gtk_widget_destroy(new_connection_window);
 		//Data Valid, start connection.
 		
 	}else{
@@ -105,32 +191,53 @@ static void connectToIp(GtkWidget *widget,GtkBuilder *data){
 	}
 }
 
-int main(int argc,char *argv[]){
-
-	gtk_init(&argc,&argv);
-
+void createNewConnectionWindow(GtkWidget *widget,gpointer data){
 //SETTING UP NEW CONNECTION WINDOW
+	gtk_widget_set_sensitive(task_manager_window,FALSE);
+
+	GtkWidget *box;
+	GtkWidget *content_area;
+
+	new_connection_window = gtk_dialog_new();
+	
 	new_connection_ui=gtk_builder_new();
 	gtk_builder_add_from_file(new_connection_ui,"new-connection-dialogue.ui",NULL);
 
-	new_connection_window = gtk_builder_get_object(new_connection_ui,"new-connection");
-	g_signal_connect(new_connection_window,"destroy",G_CALLBACK(gtk_main_quit),NULL);
-
+	box = gtk_builder_get_object(new_connection_ui,"new-connection-box");
+	
 	new_connection_okay=gtk_builder_get_object(new_connection_ui,"okay-button");
 	g_signal_connect(new_connection_okay,"clicked",G_CALLBACK(connectToIp),new_connection_ui);
 
 	new_connection_cancel=gtk_builder_get_object(new_connection_ui,"cancel-button");
-	g_signal_connect(new_connection_cancel,"clicked",G_CALLBACK(gtk_main_quit),NULL);
+	g_signal_connect(new_connection_cancel,"clicked",G_CALLBACK(closeNewConnectionWindow),new_connection_window);
+
+	content_area = gtk_dialog_get_content_area(GTK_DIALOG(new_connection_window));
+	//gtk_widget_reparent(box,GTK_CONTAINER(content_area));
+	gtk_container_add(GTK_CONTAINER(content_area), box);
+
+	g_signal_connect(new_connection_window, "destroy", G_CALLBACK(closeNewConnectionWindow), new_connection_window);
+	gtk_window_set_title(new_connection_window,"Create New Connection");
+	gtk_window_set_skip_taskbar_hint(new_connection_window,TRUE);
+	gtk_window_set_resizable(GTK_WINDOW(new_connection_window), FALSE);
 
 	gtk_widget_show_all(new_connection_window);
-//SETTING UP NEW CONNECTION WINDOW ends.	
+
+
+	
+	//gtk_widget_grab_focus(GTK_WIDGET(new_connection_window));
+//SETTING UP NEW CONNECTION WINDOW ends.
+}
+
+int main(int argc,char *argv[]){
+
+	gtk_init(&argc,&argv);
 
 //SETTING UP TASK-MANAGER WINDOW
-	/*task_manager_ui=gtk_builder_new();
+	task_manager_ui=gtk_builder_new();
 	gtk_builder_add_from_file(task_manager_ui,"task-manager-ui.ui",NULL);
 
-	window = gtk_builder_get_object(task_manager_ui,"task-manager");
-	g_signal_connect(window,"destroy",G_CALLBACK(gtk_main_quit),NULL);
+	task_manager_window = gtk_builder_get_object(task_manager_ui,"task-manager");
+	g_signal_connect(task_manager_window,"destroy",G_CALLBACK(gtk_main_quit),NULL);
 
 	menubar=gtk_builder_get_object(task_manager_ui,"menubar");
 	
@@ -146,11 +253,14 @@ int main(int argc,char *argv[]){
 	menu_help=gtk_builder_get_object(task_manager_ui,"menuitem-help");
 	g_signal_connect(menu_help,"activate",G_CALLBACK(printMenuActivatedData),"Menu-Help");
 
+	file_newcon=gtk_builder_get_object(task_manager_ui,"menu-file-new");
+	g_signal_connect(file_newcon,"activate",G_CALLBACK(createNewConnectionWindow),NULL);
+
 	file_run=gtk_builder_get_object(task_manager_ui,"menu-file-run");
 	g_signal_connect(file_run,"activate",G_CALLBACK(printMenuActivatedData),"File-Run");
 
 	file_exit=gtk_builder_get_object(task_manager_ui,"menu-file-exit");
-	g_signal_connect(file_run,"activate",G_CALLBACK(printMenuActivatedData),"File-Run");
+	g_signal_connect(file_exit,"activate",G_CALLBACK(gtk_main_quit),NULL);
 
 	options_ontop=gtk_builder_get_object(task_manager_ui,"menu-options-ontop");
 	g_signal_connect(options_ontop,"activate",G_CALLBACK(printMenuActivatedData),"Options-OnTop");
@@ -170,8 +280,9 @@ int main(int argc,char *argv[]){
 	help_about=gtk_builder_get_object(task_manager_ui,"menu-help-about");
 	g_signal_connect(help_about,"activate",G_CALLBACK(printMenuActivatedData),"Help-About");
 
-	gtk_widget_show_all(window);
-	*/
+	gtk_widget_show_all(task_manager_window);
+
+	createNewConnectionWindow(NULL,NULL);
 
 	gtk_main();
 
